e5b44e3df4f878796974d51c2d08f7e4414a2724
Bhooshan007 <bhooshans95@gmail.com>
2022-08-22 16:51:34 +0530

    added:Using switch case statement

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -1,36 +1,47 @@
 package com.bridgelabz;
 
+import java.util.Random;
+
 public class EmployeeWageProgram 
 {
-	static int is_Present = 1;
+	
+	static int is_full_time = 1;
+	static int is_part_time = 2;
 	static int wage_per_hr = 20;
 	static int full_time = 8;
 	static int part_time = 4;
-	static int empwage = 0;
 	
 	public static void main(String[] args) 
 	{
 		
 		
-		double empcheck = Math.floor(Math.random()*10)%3;
+		Random random = new Random();
+		int empPresent = random.nextInt(3);
+		System.out.println("Employee attendence:"+empPresent);
 		
-		if(empcheck == 1)
-		{
-			empwage = wage_per_hr * full_time;
-			System.out.println("Employee is present");
-			
-		}
-		else if(empcheck == 2)
-		{
-			empwage = wage_per_hr * part_time;
-			System.out.println("Employee is part time");
-		}
-		else
-		{
-			System.out.println("Employee is absent");
-		}
+		int empwage = 0;
 		
-		System.out.println("Employee wage:" +empwage);
+		switch (empPresent)
+        {
+            case 1:
+            {
+                empwage = wage_per_hr * full_time;
+                System.out.println("Employee is present and the wage is : " + empwage);
+                break;
+
+            }
+            case 2:
+            {
+                empwage = wage_per_hr * part_time;
+                System.out.println("Employee is Part time present and the wage is : " + empwage);
+                break;
+            }
+            default:
+            {
+                System.out.println("Employee is absent and the wage is : " + empwage);
+                break;
+            }
+        }
 	}
     
 }
