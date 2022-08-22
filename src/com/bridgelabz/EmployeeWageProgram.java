09099804b044cbe344abfee03b50b672a2ab9649
Bhooshan007 <bhooshans95@gmail.com>
2022-08-22 19:28:11 +0530

    added:Total working hours or days are reached per month

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -1,49 +1,48 @@
 package com.bridgelabz;
-
 import java.util.Random;
- 
+
 public class EmployeeWageProgram 
 {
-	
-	static int is_full_time = 1;
-	static int is_part_time = 2;
-	static int wage_per_hr = 20;
-	static int full_time = 8;
-	static int part_time = 4;
-	static int working_day = 20;
-	
+
 	public static void main(String[] args) 
 	{
 		
-		int empwage = 0;
-		int totalempwage = 0;
-		
-		for(int i=0; i < working_day; i++)
+        int wage_per_hr = 20;
+		int full_time = 8;
+		int part_time = 4;
+		int wage_per_day = 0;
+		int monthly_salary = 0;
+		int monthly_hour = 0;
+		int days = 0;
+
+		while (monthly_hour < 100 && days < 20) 
 		{
+
+			days++;
 			Random random = new Random();
 			int empcheck = random.nextInt(3);
-			System.out.println("Employee attendence:"+empcheck);
-			
-			switch (empcheck)
+			switch (empcheck) 
 			{
+
+			case 0:
+				System.out.println("Employee Absent");
+				break;
+
 			case 1:
-				empwage = wage_per_hr * full_time;
-				System.out.println("Employee is present and wage is:"+empwage);
+				System.out.println("Employee Part Time Present");
+				wage_per_day = part_time * wage_per_hr;
+				monthly_hour = monthly_hour + part_time;
 				break;
-				
-			case 2:
-				empwage = wage_per_hr * part_time;
-				System.out.println("Employee is part time present and wage is:"+empwage);
+			default:
+				System.out.println("Employee Full Day Present..");
+				wage_per_day = wage_per_hr * full_time;
+				monthly_hour = monthly_hour + full_time;
 				break;
-				
-				default:
-					System.out.println("Employee is absent and wage is:"+empwage);
-					break;
 			}
-			
-			totalempwage = empwage + totalempwage;
-			System.out.println("Total wage of employee for the month is : " + totalempwage);
+
+			monthly_salary = monthly_salary + wage_per_day;
+			System.out.println("Day: " + days + " MonthlyHours: " + monthly_hour + ": Monthly Salary: " + monthly_salary);
 		}
-	  }   
-    
+
+	}
 }
