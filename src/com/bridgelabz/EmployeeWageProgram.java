f892c1bfdd02c6dd547e9ea54277451498b49b58
Bhooshan007 <bhooshans95@gmail.com>
2022-08-22 15:17:09 +0530

    added:Check Employee Presence

--- src/com/bridgelabz/EmployeeWageProgram.java
+++ src/com/bridgelabz/EmployeeWageProgram.java
@@ -1,10 +1,22 @@
 package com.bridgelabz;
 
-public class EmployeeWageProgram {
+public class EmployeeWageProgram 
+{
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
-		System.out.println("Welcome To Employee Wage Program");
+		int is_Present = 1;
+		double empcheck = Math.floor(Math.random()*10)%2;
+		
+		if(empcheck == is_Present)
+		{
+			System.out.println("Employee is present");
+		}
+		else
+		{
+			System.out.println("Employee is absent");
+		}
 	}
 
 }
